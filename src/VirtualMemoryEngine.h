e4c6bdc42fb16315fe018fd6b61004706daacad6
Zoran Stepanoski <zstepanoski@gmail.com>
2026-08-07 22:54:18 +0200

    Add files via upload

---
+++ src/VirtualMemoryEngine.h
@@ -0,0 +1,47 @@
+#ifndef VIRTUAL_MEMORY_ENGINE_H
+#define VIRTUAL_MEMORY_ENGINE_H
+
+#include <Arduino.h>
+
+// Forward deklaracija - drži unutrašnju arhitekturu potpunom tajnom od klijenta (Pimpl Pattern)
+class VirtualMemoryEngineImpl;
+
+class VirtualMemoryEngine {
+private:
+    VirtualMemoryEngineImpl* pImpl; // Opaque pointer na tajnu implementaciju
+
+public:
+    // Konstruktor: (veličina_bloka u bajtovima, max_ram_pool u bajtovima, max_blokova_u_memoriji)
+    VirtualMemoryEngine(size_t blockSize = 32768, size_t ramLimit = 131072, int maxBlocks = 16);
+    ~VirtualMemoryEngine();
+
+    // Inicijalizacija sistema i LittleFS paging-a
+    bool begin();
+
+    // Bazične operacije za rad sa sirovim bajtovima
+    void read(uint32_t address, void* dest, size_t size);
+    void write(uint32_t address, const void* src, size_t size);
+    void flush(); // Prisilno pražnjenje keša na disk
+
+    // Vraća tačnu veličinu svih komprimovanih podataka u bajtovima
+    size_t getCompressedSize();
+
+    // Vraća stvarni algebarski faktor kompresije (ne zavisi od sektora čipa)
+    float getCompressionRatio(size_t totalVirtualSize);
+
+    // Templatizovana funkcija za brz upis bilo kojeg tipa/strukture
+    template <typename T>
+    void put(uint32_t address, const T& value) {
+        write(address, &value, sizeof(T));
+    }
+
+    // Templatizovana funkcija za brzo čitanje bilo kojeg tipa/strukture
+    template <typename T>
+    T get(uint32_t address) {
+        T value;
+        read(address, &value, sizeof(T));
+        return value;
+    }
+};
+
+#endif // VIRTUAL_MEMORY_ENGINE_H
